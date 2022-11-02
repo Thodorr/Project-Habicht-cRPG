69d4d6eaff2bc048e9c28be58d59886cc0108051
Tom Kertels <tom.kertels@mni.thm.de>
2022-11-02 20:24:34 +0100

    Add animation to character

--- Charakter.gd
+++ Charakter.gd
@@ -1,14 +1,39 @@
 extends KinematicBody2D
 
+enum State {
+	MOVING,
+	IDLE,
+	LOOTING,
+	INTERACTING
+}
 
 onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+onready var animation_tree: AnimationTree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
 
 var may_move = false
+var state = State.IDLE
+var direction = Vector2(0,0)
 
 func _ready():
 	nav_agent.connect("velocity_computed", self, "_on_velocity_computed")
+	animation_state.start("Idle")
+
+func state_handler():
+	if nav_agent.is_navigation_finished():
+		state = State.IDLE
+
+func animation_player():
+	if state == State.MOVING:
+		animation_tree.set("parameters/Idle/blend_position", direction)
+		animation_tree.set("parameters/Walk/blend_position", direction)
+		animation_state.travel("Walk")
+	else: 
+		animation_state.travel("Idle")
+
 
 func _on_velocity_computed(velocity):
+	state = State.MOVING
 	move_and_slide(velocity)
 
 func set_velocity(): 
@@ -17,7 +42,7 @@ func set_velocity():
 		return
 	
 	var targetpos: Vector2 = nav_agent.get_next_location()
-	var direction: Vector2 = global_position.direction_to(targetpos)
+	direction = global_position.direction_to(targetpos)
 	var velocity: Vector2 = direction * nav_agent.max_speed
 	nav_agent.set_velocity(velocity)
 
@@ -29,3 +54,5 @@ func _unhandled_input(_event):
 func _physics_process(_delta):
 	if may_move:
 		set_velocity()
+		state_handler()
+		animation_player()
