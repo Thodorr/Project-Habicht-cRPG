4fd529760e0daeaa697600a7e7df2811e277c580
TimatEdu <117275631+TimatEdu@users.noreply.github.com>
2022-12-21 08:36:22 +0100

    Change to collision from button
    Changes the Scene, when the player runs in to a collisionobject.

--- Engine/ChangeLevel.gd
+++ Engine/ChangeLevel.gd
@@ -1,8 +1,8 @@
-extends Control
+extends Node
 
 
 func _ready():
 	pass
 
-func _on_Button_pressed():
-	scenechanger.goto_scene("res://Level/level_1/level_1_entrance.tscn")
+func _on_Area2D_body_entered(_body):
+	scenechanger.change_("res://Level/level_1/level_1_entrance.tscn")
