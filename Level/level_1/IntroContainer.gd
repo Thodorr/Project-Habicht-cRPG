566729fe997c3dc5f1a5039321ef238ee4a99850
Cr0m3t0r <clemens.vincent.theodor.hawickenbrauck@mni.thm.de>
2023-01-11 15:29:39 +0100

    Merge Conflict
    Conflict resolve

---
+++ Level/level_1/IntroContainer.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+
+func _ready():
+	que_intro()
+
+func que_intro():
+	var dialog = Dialogic.start("Intro")
+	dialog.connect('dialogic_signal', self, '_end_intro')
+	add_child(dialog)
+	print(get_children())
+	get_node("CanvasLayer").visible = true
+
+func _end_intro(context):
+	if context == 'end_intro':
+		var dialog_node = get_child(2)
+		remove_child(dialog_node)
+		get_child(0).play("hide_black_screen")
+		#que_end()
+
+func que_end():
+	print ("end intro")
+	print (get_path())
+	get_child(0).play("hide_black_screen")
+	var npc_pos1 = get_node("../../YSort/NPC2")
+	npc_pos1.set_position(Vector2(273, 217))
