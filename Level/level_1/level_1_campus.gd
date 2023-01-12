415bbe36bd611e61a44c832d44b33429e3b33a49
TimatEdu <117275631+TimatEdu@users.noreply.github.com>
2023-01-12 08:24:53 +0100

    Loop of noise with new ogg file
    Changed to ogg file and loop noise in intro

--- Level/level_1/level_1_campus.gd
+++ Level/level_1/level_1_campus.gd
@@ -6,17 +6,9 @@ extends Node2D
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	var player = get_node("YSort/Charakter")
 	player.nav_agent.set_target_location(Vector2(280,100))
 	player.set_position(Vector2(280,100))
 	$music.play()
-	
-	
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
