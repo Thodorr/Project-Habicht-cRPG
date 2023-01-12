d9c15b9b9810a08e2bec69a36d1e222b67c88753
Tom Kertels <tom.kertels.1@gmail.com>
2023-01-12 20:44:33 +0100

    Add missing camera values

--- Level/level_1/intro_area.gd
+++ Level/level_1/intro_area.gd
@@ -3,6 +3,11 @@ extends Node2D
 onready var size_of_map = get_node("camera_area/CollisionShape2D").shape
 onready var camera = get_node("YSort/Charakter/Camera2D")
 
+export var border_left = 0
+export var border_right = 464
+export var border_top = 0
+export var border_bottom = 549
+
 func _ready():
 	var spawn = $Spawn.get_position()
 	var player = get_node("YSort/Charakter")
